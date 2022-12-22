edc6c88e784309589b1ee3b4f3d42336f9ac6002
Wilsvn <wilsonvposcablo@gmail.com>
2022-12-22 09:52:57 -0500

    w

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,10 +1,1 @@
 import "../styles/style.css";
-
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Hello</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>`;
-
-setupCounter(document.querySelector("#counter"));
